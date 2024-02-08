6e723cc1fd1b5f409e0188858a09eeab1051a41e
Felicia <felicia.lyden@gmail.com>
2024-02-08 20:52:00 +0100

    add navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BoardComponent } from './board/board.component';
 import { PlayerComponent } from './player/player.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BoardComponent, PlayerComponent],
+  imports: [BoardComponent, PlayerComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
