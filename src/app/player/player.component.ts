50c9d9103194efa6a8597335d9bd257ba7ad8060
Felicia <felicia.lyden@gmail.com>
2024-02-08 20:20:52 +0100

    add input for player number

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-player',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './player.component.css'
 })
 export class PlayerComponent {
-
+  @Input() playerNo! : number
 }
