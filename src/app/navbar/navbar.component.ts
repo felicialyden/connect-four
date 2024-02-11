b17249bfaad42880a766e46c009eb079b90ecc2f
Felicia <felicia.lyden@gmail.com>
2024-02-11 14:44:24 +0100

    add event listeners to nav btns

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  onSeeRules() {
+    console.log("see rules")
+  }
+  onRestart() {
+    console.log("restart")
+  }
 }
