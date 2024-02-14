ac303d77418c63ce547547ee2bf3ebbebd816093
Felicia <felicia.lyden@gmail.com>
2024-02-14 19:04:52 +0100

    send correct id when clicking svg

--- src/app/board-nav/board-nav.component.ts
+++ src/app/board-nav/board-nav.component.ts
@@ -13,6 +13,8 @@ export class BoardNavComponent {
 
   
   onClickBoardSquare(event: MouseEvent) {
-    this.chosenLine.emit(Number((event.target as HTMLDivElement).id));
+    const lineId = (event.target as HTMLElement).closest(".board-nav-item")?.id
+    console.log(lineId)
+    this.chosenLine.emit(Number(lineId));
 }
 }
